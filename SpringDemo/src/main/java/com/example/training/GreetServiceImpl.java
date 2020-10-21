55a8ba2ffb146767420fb339334a07acf2edb119
99002756 <72467067+99002756@users.noreply.github.com>
2020-10-21 17:21:35 +0530

    Add files via upload

---
+++ SpringDemo/src/main/java/com/example/training/GreetServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.training;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetServiceImpl implements GreetService {
+
+	@Override
+	public String greet(String name) {
+		// TODO Auto-generated method stub
+		return name;
+	}
+	
+
+}
