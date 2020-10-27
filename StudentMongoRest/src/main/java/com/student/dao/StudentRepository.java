4f1d4619978e0f9325fc36251a40c83f78b5ec46
99002756 <72467067+99002756@users.noreply.github.com>
2020-10-27 22:18:57 +0530

    Update StudentRepository.java

--- StudentMongoRest/src/main/java/com/student/dao/StudentRepository.java
+++ StudentMongoRest/src/main/java/com/student/dao/StudentRepository.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-
+import org.springframework.stereotype.Repository;
 import com.student.model.Student;
-
+import com.studentapp.exceptions.StudentNotFoundException;
 public interface StudentRepository extends MongoRepository<Student,Integer>{
-	List<Student> findByAddressCity(String city);
-	List<Student> findStudentsByDept(String dep);
+	List<Student> findByAddressCity(String city)throws StudentNotFoundException;
+	List<Student> findStudentsByDept(String dep)throws StudentNotFoundException;
 	
 	
 	//@Query("from Student b where b.age>?20")
