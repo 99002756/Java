db55ba2dbfbd40fbdf88d6b111dbe69e0bb0094c
99002756 <72467067+99002756@users.noreply.github.com>
2020-10-27 22:16:03 +0530

    Update StudentNotFoundException.java

--- StudentMongoRest/src/main/java/com/student/exception/StudentNotFoundException.java
+++ StudentMongoRest/src/main/java/com/student/exception/StudentNotFoundException.java
@@ -7,25 +7,11 @@ public class StudentNotFoundException extends Exception{
 		super();
 	}
 
-	public StudentNotFoundException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
-
-	public StudentNotFoundException(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
-
+	
 	public StudentNotFoundException(String message) {
 		super(message);
 		// TODO Auto-generated constructor stub
 	}
 
-	public StudentNotFoundException(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
-	}
 
 }
