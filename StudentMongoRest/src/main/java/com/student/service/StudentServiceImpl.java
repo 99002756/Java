a40d9df098410594c3ec9569cb753db57a037191
99002756 <72467067+99002756@users.noreply.github.com>
2020-10-27 22:02:31 +0530

    Update StudentServiceImpl.java

--- StudentMongoRest/src/main/java/com/student/service/StudentServiceImpl.java
+++ StudentMongoRest/src/main/java/com/student/service/StudentServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
 import com.student.dao.StudentRepository;
 import com.student.exception.StudentNotFoundException;
 import com.student.model.Student;
 @Service
+@Repository
 public class StudentServiceImpl implements StudentService{
 	@Autowired
 	StudentRepository studentRepository;
